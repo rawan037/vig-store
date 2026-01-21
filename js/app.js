cc9e0576e1c90f84ba689ee6a02612de477a7414
rawan037 <rawanbaniamer037@gmail.com>
2026-01-21 10:49:01 +0300

    Update app.js
    Signed-off-by: rawan037 <rawanbaniamer037@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const orderId = "152"; // رقم الطلب لتجربة
+const orderRef = doc(window.db, "orders", orderId);
 
+onSnapshot(orderRef, (docSnap) => {
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    document.getElementById("orderStatus").innerText = data.status;
+
+    const driverLocation = data.driverLocation;
+    if (driverLocation && window.map) {
+      new google.maps.Marker({
+        position: driverLocation,
+        map: window.map,
+        label: "🚚",
+      });
+      window.map.setCenter(driverLocation);
+    }
+  }
+});
