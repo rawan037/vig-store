fcbbb0b9933bd14dacd427981bad2b9e28117d8f
rawan037 <rawanbaniamer037@gmail.com>
2026-02-23 02:18:34 +0300

    Update save_invoice.php
    Signed-off-by: rawan037 <rawanbaniamer037@gmail.com>

--- save_invoice.php
+++ save_invoice.php
@@ -1,1 +1,22 @@
+<?php
+$conn = new mysqli("localhost","DB_USER","DB_PASS","invoices_db");
+$data = json_decode(file_get_contents("php://input"), true);
 
+$stmt = $conn->prepare("INSERT INTO invoices 
+(invoice_number,customer_name,customer_phone,total,items) 
+VALUES (?,?,?,?,?)");
+
+$items = json_encode($data['items']);
+
+$stmt->bind_param("sssds",
+$data['invoice_number'],
+$data['customer_name'],
+$data['customer_phone'],
+$data['total'],
+$items
+);
+
+$stmt->execute();
+echo "Saved";
+$conn->close();
+?>
